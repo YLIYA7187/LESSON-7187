0a4c7d6333db837cee7a7b69a3a3d0a5e68707b6
YLIYA7187 <yuliya.vymorkova@mail.ru>
2026-02-16 18:31:42 +0300

    Добавлена папка lesson1 с файлами

---
+++ lesson1/lesson1.py
@@ -0,0 +1,14 @@
+def mines(a, b):
+    return a - b
+
+
+v = mines(40, 2)  # Правильный вызов функции с круглыми скобками
+print(v)
+
+
+def delenie(d, n):
+    return d / n
+
+
+f = delenie(100000000, 6)
+print(f)
